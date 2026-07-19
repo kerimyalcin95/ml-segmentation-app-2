16b91e354e337410055190c60d11228617426d49
Kerim Yalcin <kerimyalcin.95@it-yalc.in>
2026-07-19 19:20:25 +0200

    refactor: streamline WebSocket handler and improve connection logging

--- python/server.py
+++ python/server.py
@@ -3,14 +3,19 @@ import asyncio
 import websockets
 
 async def handler(websocket):
-    async for message in websocket:
-        print(f"Received from Electron: {message}")
-        # Send a response back
-        await websocket.send(f"{message}")
+    print("Connected")
+
+    try:
+        async for message in websocket:
+            print(f"'{message}'")
+            await websocket.send(message)
+
+    except websockets.exceptions.ConnectionClosed as e:
+        print(f"Connection closed. \n Error message: {e.code}\n Reason message: {e.reason}")
 
 async def main():
     async with websockets.serve(handler, "localhost", 8765):
-        print("WebSocket server listening on ws://localhost:8765")
+        print("Listening on ws://localhost:8765")
         await asyncio.Future() 
 
 if __name__ == "__main__":
