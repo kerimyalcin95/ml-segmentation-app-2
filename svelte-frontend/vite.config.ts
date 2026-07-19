969ae9b6baaf5e8e30d488dd0bd7e3d7166c0920
Kerim Yalcin <kerimyalcin.95@it-yalc.in>
2026-07-19 19:28:30 +0200

    chore: update package versions and improve build scripts

--- svelte-frontend/vite.config.ts
+++ svelte-frontend/vite.config.ts
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    svelte()
+    plugins: [
+        tailwindcss(),
+        svelte()
     ],
-  base: './',
-  build: {
-    outDir: './dist',
-    assetsDir: './assets',
-  },
-  resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
+    base: './',
+    build: {
+        outDir: './dist',
+        assetsDir: './assets',
+        emptyOutDir: true,
     },
-  },
-  define: {
+    resolve: {
+        alias: {
+            $lib: path.resolve("./src/lib"),
+        },
+    },
+    define: {
         __BUILD_TIME__: JSON.stringify(new Date().toLocaleString())
-    }
-})
+    },
+});
