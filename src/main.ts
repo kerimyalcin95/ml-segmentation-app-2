c0dbdd2fc605c5d1b8db02dfc8b41ac6d3e37406
Kerim Yalcin <kerimyalcin.95@it-yalc.in>
2026-07-19 19:19:05 +0200

    feat: implement WebSocket connection and Python server integration

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 import { app, BrowserWindow, ipcMain, IpcMainEvent } from 'electron';
 import path from 'node:path';
+import { fileURLToPath } from "node:url";
 import { spawn, ChildProcessWithoutNullStreams } from 'node:child_process';
 import WebSocket from 'ws';
 
 let webSocket: WebSocket | undefined;
 let pythonProcess: ChildProcessWithoutNullStreams | undefined;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // distinguish between development and production environment
 const pythonPath: string = app.isPackaged
     ? path.join(
-          process.resourcesPath,
-          'app.asar.unpacked',
-          'python',
-          'server.py'
-      )
+        process.resourcesPath,
+        'app.asar.unpacked',
+        'python',
+        'server.py'
+    )
     : path.join(__dirname, '..', '..', 'python', 'server.py');
 
 const createWindow = (): void => {
-    
+
     const window = new BrowserWindow({
-        width: 800,
-        height: 800,
+        autoHideMenuBar: true,
         webPreferences: {
             preload: path.join(__dirname, 'preload.cjs'),
             contextIsolation: true,
@@ -28,36 +31,47 @@ const createWindow = (): void => {
         },
     });
 
+    window.webContents.on("before-input-event", (event, input) => {
+        if (input.key === "F12") {
+            window.webContents.toggleDevTools();
+        }
+    });
+
+    window.maximize();
     window.loadFile(path.join(__dirname, '..', '..', 'html', '../', 'svelte-frontend', 'dist', 'index.html'));
 };
 
 const connectToPythonServer = (): void => {
     webSocket = new WebSocket('ws://localhost:8765');
+    console.log('Electron: Listening on ws://localhost:8765');
 
     webSocket.on('open', () => {
-        console.log('Connected to Python server');
-        webSocket?.send('Hello from Electron main process!');
+        console.log('Electron: Connected');
     });
 
     webSocket.on('message', (data) => {
-        console.log(`Received from Python: ${data}`);
+        console.log(`Python: ${data}`);
         const win = BrowserWindow.getAllWindows()[0];
         if (win && !win.webContents.isLoading()) {
             win.webContents.send('update-button', data.toString());
-            console.log(`Sending [${data.toString()}] to IPC Electron`);
         }
     });
 
-    webSocket.on('close', () => console.log('Connection closed'));
-    webSocket.on('error', (err) => console.error('WebSocket error:', err));
+    webSocket.on('close', (code, reason) => {
+        console.log(`WebSocket closed: ${code}, ${reason.toString()}`);
+    });
+
+    webSocket.on('error', (err) => {
+        console.error('WebSocket error:', err);
+    });
 };
 
 const startPythonServer = (): void => {
     pythonProcess = spawn('python', ['-u', pythonPath]);
-    console.log("Python server started.");
+    console.log("Python: Server started.");
 
-    pythonProcess.stdout.on('data', (data) => console.log(`Py: ${data}`));
-    pythonProcess.stderr.on('data', (data) => console.error(`PyErr: ${data}`));
+    pythonProcess.stdout.on('data', (data) => console.log(`Python: ${data}`));
+    pythonProcess.stderr.on('data', (data) => console.error(`Python Error: ${data}`));
 };
 
 const stopPythonServer = (): void => {
